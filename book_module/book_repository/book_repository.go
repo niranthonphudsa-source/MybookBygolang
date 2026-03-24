60d0f98504d8c11630091a70b4e3cf146dfd84bb
niranth-thon <niran.thonphudsa@gmail.com>
2026-03-24 18:18:56 +0700

    fix: Edit Code Error

--- book_module/book_repository/book_repository.go
+++ book_module/book_repository/book_repository.go
@@ -50,9 +50,8 @@ func (conn *getDbBooks) GetBooksAll() ([]entities.Books, error) {
 
 func (conn *getDbBooks) GetBookId(book_id int) (*entities.Books, error) {
 	var b entities.Books
-	err := conn.db.QueryRow("SELECT book_id, book_name,"+
-		"book_author, admibupdate_id FROM public.mybook"+
-		"_db WHERE book_id=$1", book_id).Scan(b.Book_id, b.Book_name, b.Book_author, b.Adminupdate_id)
+	err := conn.db.QueryRow("SELECT book_id, book_name, book_author, adminupdate_id "+
+		" FROM public.mybook_db WHERE book_id = $1;", book_id).Scan(&b.Book_id, &b.Book_name, &b.Book_author, &b.Adminupdate_id)
 	if err != nil {
 		return nil, err
 	}
