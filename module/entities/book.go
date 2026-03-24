7b77f52f527f11f7960010bfb1f097c1bc583252
niranth <niran.thonphudsa@gmail.com>
2026-03-24 17:17:21 +0700

    Delete module directory

--- module/entities/book.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type Books struct {
-	Book_id        int    `json:"book_id"`
-	Book_name      string `json:"book_name"`
-	Book_author    string `json:"book_author"`
-	Adminupdate_id string `json:"adminupdate_id"`
-}
